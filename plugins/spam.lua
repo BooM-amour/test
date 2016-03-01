225852da428824150a7f2051a5915cb1ebafe8f2
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-01 10:17:27 -0800

    Added files via upload

--- plugins/spam.lua
+++ plugins/spam.lua
@@ -1,50 +1,28 @@
---------------------------------------------------
---      ____  ____ _____                        --
---     |    \|  _ )_   _|___ ____   __  __      --
---     | |_  )  _ \ | |/ ·__|  _ \_|  \/  |     --
---     |____/|____/ |_|\____/\_____|_/\/\_|     --
---                                              --
---------------------------------------------------
---                                              --
---       Developers: @Josepdal & @MaSkAoS       --
---     Support: @Skneos,  @iicc1 & @serx666     --
---                                              --
---------------------------------------------------
+do
 
-local function send_report(msg)
-    local text = '👤 '..lang_text(msg.to.id, 'reportUser')..': '..msg.from.username..' ('..msg.from.id..')\n‼ '..lang_text(msg.to.id, 'reportReason')..': Spam\n💬 '..lang_text(msg.to.id, 'reportGroup')..': "'..msg.to.title..'" ('..msg.to.id..')\n✉ '..lang_text(msg.to.id, 'reportMessage')..': '..msg.text
-    for v,user in pairs(_config.sudo_users) do
-        send_msg('user#id'..user, text, ok_cb, true)
-    end
+function run(msg, matches)
+       if not is_sudo(msg) then
+              return "هوی اسگل نزن :)"       end
+  local tex = matches[1]
+  local sps = matches[2]
+  local sp = 'تموم شد بابایی راند برای راند بعدی امادم'
+
+for i=1, tex, 1 do
+
+sp = '\n'..sps..'\n'..sp
+i = i + 1
+
 end
 
-local function kick_user(msg)
-    local chat = 'chat#id'..msg.to.id
-    local channel = 'channel#id'..msg.to.id
-    local user = msg.from.id
-    if msg.to.type == 'chat' then
-        chat_del_user(chat, 'user#id'..user, ok_cb, true)
-    elseif msg.to.type == 'channel' then
-        channel_kick_user(channel, 'user#id'..user, ok_cb, true)
-    end
-end
+return sp
 
-local function run(msg, matches)
-    if not permissions(msg.from.id, msg.to.id, "settings") then
-        local hash = 'spam:'..msg.to.id
-        if redis:get(hash) then
-            kick_user(msg)
-            delete_msg(msg.id, ok_cb, false)
-            send_report(msg)
-        end
-    end
 end
 
 return {
-patterns = {
-    -- You can add much as patterns you want to stop all spam traffic
-    "[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]",
-    "[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Oo][Rr][Gg]",
-    "[Aa][Dd][Ff].[Ll][Yy]",
-    "[Bb][Ii][Tt].[Ll][Yy]"
-}, run = run}
+    patterns = {
+      "[!/#$&@ ]spam (.*) (.*)$"
+    },
+    run = run,
+}
+
+end
--- plugins/version.lua
+++ plugins/version.lua
@@ -1,27 +1,15 @@
---------------------------------------------------
---      ____  ____ _____                        --
---     |    \|  _ )_   _|___ ____   __  __      --
---     | |_  )  _ \ | |/ ·__|  _ \_|  \/  |     --
---     |____/|____/ |_|\____/\_____|_/\/\_|     --
---                                              --
---------------------------------------------------
---                                              --
---       Developers: @vvViRuSss & xX_mahtab_xX      --
---     Support: @vvViRuSss    --
---                                              --
---------------------------------------------------
-
 do
 
 function run(msg, matches)
-  return 'ViRuSteam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttps://github.com/BooM-amour/test\n\nDevelopers :\n@vvViRuSss\n@xX_mahtab_xX\n\nSupport :\n@xX_mahtab_xX\nvvViRuSss\n@zvirusx' 
-  --[[ Checkout https://github.com/BooM-amour/test
-  --GNU GPL v2 license.]]
+  return 'pixels Bot '.. VERSION .. [[ 
+  Checkout v0.1 license.]]
 end
 
 return {
+  description = "Shows bot version", 
+  usage = "version: Shows bot version",
   patterns = {
-    "^#AmouR$"
+    "^[!/#$&@ ]version$"
   }, 
   run = run 
 }
