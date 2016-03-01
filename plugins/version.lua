108ff850ec05a1a31267b4519a71943da2ed7cd6
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-01 10:00:18 -0800

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -6,22 +6,22 @@
 --                                              --
 --------------------------------------------------
 --                                              --
---       Developers: @Josepdal & @MaSkAoS       --
---     Support: @Skneos,  @iicc1 & @serx666     --
+--       Developers: @vvViRuSss & xX_mahtab_xX      --
+--     Support: @vvViRuSss    --
 --                                              --
 --------------------------------------------------
 
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttp://github.com/Josepdal/DBTeam\n\nDevelopers :\n@MaSkAoS\n@Josepdal\n\nSupport :\n@skneos\n@iicc1\n@Serx666' 
-  --[[ Checkout https://github.com/Josepdal/DBTeam
+  return 'ViRuSteam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttps://github.com/BooM-amour/test\n\nDevelopers :\n@vvViRuSss\n@xX_mahtab_xX\n\nSupport :\n@xX_mahtab_xX\nvvViRuSss\n@zvirusx' 
+  --[[ Checkout https://github.com/BooM-amour/test
   --GNU GPL v2 license.]]
 end
 
 return {
   patterns = {
-    "^#version$"
+    "^#AmouR$"
   }, 
   run = run 
 }
